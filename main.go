ee847aff01302c7a627643bbf86dae86d8c03d79
sawilkhan <68275664+sawilkhan@users.noreply.github.com>
2024-05-22 18:52:57 +0530

    set up config files and created init functions for the app and mongoDB

--- main.go
+++ main.go
@@ -1,13 +1,57 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/sawilkhan/go-rest/database"
+)
 
 func main() {
+	//init app
+	err := initApp()
+	if err != nil{
+		panic(err)
+	}
+
+	//start app
+	defer database.CloseMongoDB()
+
+
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
+	app.Post("/", func(c *fiber.Ctx) error {
+		//write a todo to the database
+
+
 		return c.SendString("Hello, World!")
 	})
 
 	app.Listen(":3000")
+}
+
+
+func initApp() error{
+	//setup environment
+	err := loadENV()
+	if err != nil{
+		return err
+	}
+
+	//setup database
+	err = database.StartMongoDB()
+	if err != nil{
+		return err
+	}
+	return nil
+}
+
+
+func loadENV() error{
+	err := godotenv.Load()
+
+	if err != nil{
+		return err
+	}
+
+	return nil
 }
